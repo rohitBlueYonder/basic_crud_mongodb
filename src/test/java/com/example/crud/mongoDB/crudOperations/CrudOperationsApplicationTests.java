c98d58fb025019c9ea839440497af386ef9ee23f
rohitBlueYonder <rohit.rai@jda.com>
2022-10-14 15:03:47 +0530

    Connecting to mongoDB local instance

---
+++ src/test/java/com/example/crud/mongoDB/crudOperations/CrudOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud.mongoDB.crudOperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
